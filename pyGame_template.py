b26cfd33a368464ab41bfe25384bdd7bec50969e
python directorygit config --global user.name python <Platniumfrostchill@gmail.com>
2021-04-21 09:48:43 -0600

    hurry up!

---
+++ pyGame_template.py
@@ -0,0 +1,23 @@
+# Pygame template - skeleton for a new pygame project
+
+
+# initialize pygame and create window
+
+
+
+# Game loop
+running = True
+while running:
+    # keep loop running at the right speed
+
+    # Process input (events)
+
+
+    # Update
+
+
+    #Draw / render
+
+
+
+pg.quit()
